13671c617b782dbad62b00639682a1af6c1df17a
Sebastien Gougeaud <s.vallerault@gmail.com>
2012-10-16 14:17:55 +0200

    Blocking 2

--- kernel_c.c
+++ kernel_c.c
@@ -1,5 +1,7 @@
 #include <string.h>
 
+#define MIN(x,y) ( (x<y) ? x : y)
+
 void
 sgemm_c (int n,
 		 float a [n][n],
@@ -16,17 +18,17 @@ sgemm_c (int n,
 
 	memset (c, 0, n * n * sizeof (float) );
 
-	for (k = 0; k+3 < n; k+=4)
+	for (k = 0; k < n; k+=100)
 	{
-		for (i = 0; i+3 < n; i+=4)
+		for (i = 0; i < n; i+=100)
 		{
-			for (j = 0; j+3 < n; j+=4)
+			for (j = 0; j < n; j+=100)
 			{
-				for (l = k; l < k+4; ++l)
+				for (l = k; l < MIN (n, k+100); ++l)
 				{
-					for (m = i; m < i+4; ++m)
+					for (m = i; m < MIN (n, i+100); ++m)
 					{
-						for (o = j; o < j+4; ++o)
+						for (o = j; o < MIN (n, j+100); ++o)
 						{
 							c [m][o] += a [m][l] * b [l][o];
 						}
